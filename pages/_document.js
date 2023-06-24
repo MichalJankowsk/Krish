c5c6c8e3b7bb7150e98c0c881ea2c83f3fc1055e
MichalJankowsk <michal.jankowski003@gmail.com>
2023-06-24 12:46:50 +0500

    Add MetaImage (#34)

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,40 @@ const Document = () => (
         href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@500;700&family=Fira+Code:wght@400;500&family=Preahvihear&display=swap"
         rel="stylesheet"
       />
+
+      <meta name="description" content="I'm a Graphic Designer (Ui/UX)!" />
+
+      <meta name="author" content="Krishna Sharma" />
+      <meta
+        property="og:title"
+        content="Top Rated UI/UX Designer - Krish4alex"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://www.https://krish4alex.vercel.app/"
+      />
+      <meta
+        property="og:description"
+        content="I'm a Graphic Designer (Ui/UX)!"
+      />
+      <meta
+        property="og:image"
+        content="https://https://krish4alex.vercel.app/assets/png/metaimage.png"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:title"
+        content="Top Rated UI/UX Designer - Krish4alex"
+      />
+      <meta
+        property="twitter:description"
+        content="I'm a Graphic Designer (Ui/UX)!"
+      />
+      <meta
+        property="twitter:image"
+        content="https://https://krish4alex.vercel.app/assets/png/metaimage.png"
+      />
     </Head>
     <body>
       <Main />
